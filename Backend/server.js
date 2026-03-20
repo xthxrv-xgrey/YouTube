83d34f6b8aee0b934b9bae81b9e2472432404d96
Atharv Agrey <xthxrv.xgrey@gmail.com>
2026-03-20 14:33:11 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import app from "./src/app.js";
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
