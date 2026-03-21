3a4f27573abef3b93f0017b266d33ce7c64a3c3f
Atharv Agrey <xthxrv.xgrey@gmail.com>
2026-03-21 14:00:05 +0530

    feat: Implement initial application structure with basic routing, header, and authentication pages.

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,11 +3,11 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <h1>Home</h1>
       <Link to="/auth/register">Register</Link>
       <Link to="/auth/login">Login</Link>
-    </div>
+    </>
   );
 };
 
