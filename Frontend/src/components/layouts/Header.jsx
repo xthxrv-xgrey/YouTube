98702342e02257912401f1f1dee8c4a9c1d33a63
Atharv Agrey <xthxrv.xgrey@gmail.com>
2026-03-21 16:52:16 +0530

    feat: Implement initial header UI with logo and search bar, and establish Redux for settings management.

--- Frontend/src/components/layouts/Header.jsx
+++ Frontend/src/components/layouts/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
               height="24"
               viewBox="0 0 24 24"
               width="24"
-              className="fill-white"
+              className="fill-black dark:fill-white"
             >
               <path d="M0 0h24v24H0z" fill="none"></path>
               <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
