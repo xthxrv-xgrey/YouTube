3ff650adfc63094a63cbb5e144ac6f1ec4f7bf8a
Atharv Agrey <xthxrv.xgrey@gmail.com>
2026-03-25 17:51:52 +0530

    feat: Implement Multer middleware for file uploads and configure Vite with proxy, React, and Tailwind CSS.

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": "http://localhost:4000",
+    },
+  },
   plugins: [react(), tailwindcss()],
 });
